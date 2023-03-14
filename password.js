334b3815d658372383b6a95a48367ae7767d0eaf
Mp3949 <127684715+Mp3949@users.noreply.github.com>
2023-03-14 23:00:35 +0530

    Delete password.js

--- password.js
+++
@@ -1,7 +0,0 @@
-function checkpass(){
-    if(document.getElementById('password').value!=document.getElementById('c-password').value){
-        document.getElementById('al').classList.remove('d-none');
-    }else{
-        document.getElementById('al').classList.add('d-none');
-    }
-}
